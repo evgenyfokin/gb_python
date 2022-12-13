bf6eccebc68c2803dd74bd5cae29d98c3ece72f3
evgenyfokin <fokin_e@inbox.ru>
2022-12-13 23:39:40 +0300

    compleated homework 1

---
+++ task_1.py
@@ -0,0 +1,6 @@
+# task_1
+first_var = 404
+second_var = "Merhaba"
+third_var = int(input("Enter your number: "))
+forth_var = input("Enter your name: ")
+print(first_var, second_var, third_var, forth_var)
---
+++ task_5.py
@@ -0,0 +1,9 @@
+# task_5
+
+income = int(input("Income: "))
+expenses = int(input("Expenses: "))
+result = income - expenses
+if result < 0:
+    print(f'Unprofitable ({result});')
+else:
+    print(f'Profitable ({result}); Benefit: {result / income * 100}%')
